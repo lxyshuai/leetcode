32675c2fe53db1e39c38089a6a216264ae1b6aef
lxy <442536013@qq.com>
2018-11-15 00:26:04 +0800

    429

---
+++ 429. N-ary Tree Level Order Traversal.py
@@ -0,0 +1,55 @@
+"""
+Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example, given a 3-ary tree:
+
+
+
+
+
+
+
+We should return its level order traversal:
+
+[
+     [1],
+     [3,2,4],
+     [5,6]
+]
+"""
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        queue = [root]
+        this_level_count = 1
+        next_level_count = 0
+        result = []
+        level_list = []
+        while queue:
+            current_node = queue.pop(0)
+            level_list.append(current_node.val)
+            this_level_count -= 1
+            for children in current_node.children:
+                queue.append(children)
+                next_level_count += 1
+            if this_level_count == 0:
+                result.append(level_list)
+                level_list = []
+                this_level_count = next_level_count
+                next_level_count = 0
+        return result
