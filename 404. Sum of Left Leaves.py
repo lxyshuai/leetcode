4438fa9c0e672cf901eeeb0eed51209dd67023d5
lxy <442536013@qq.com>
2018-11-15 22:24:46 +0800

    404

---
+++ 404. Sum of Left Leaves.py
@@ -0,0 +1,45 @@
+"""
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def __init__(self):
+        self.left_sum = 0
+
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        def process(root, parent):
+            if root is None:
+                return
+            if parent.left == root and root.right is None and root.left is None:
+                self.left_sum += root.val
+            process(root.left, root)
+            process(root.right, root)
+
+        if root:
+            process(root.left, root)
+            process(root.right, root)
+        return self.left_sum
