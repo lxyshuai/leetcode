51a265b32b10d31763c79bdd4d366c23e30617eb
lxy <442536013@qq.com>
2018-11-17 17:46:51 +0800

    101

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,70 @@
+"""
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+        def is_mirror(t1, t2):
+            if t1 is None and t2 is None:
+                return True
+            if t1 is None or t2 is None:
+                return False
+            return t1.val == t2.val and is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left)
+
+        return is_mirror(root, root)
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        p = [root]
+        q = [root]
+        while p and q:
+            p_current_node = p.pop()
+            q_current_node = q.pop()
+            if p_current_node is None and q_current_node is None:
+                continue
+            if p_current_node is None or q_current_node is None:
+                return False
+            if p_current_node.val != q_current_node.val:
+                return False
+            p.append(p_current_node.left)
+            p.append(p_current_node.right)
+            q.append(q_current_node.right)
+            q.append(q_current_node.left)
+        return True
