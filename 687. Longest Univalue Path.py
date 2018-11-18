dc1532c0b2194841678328a545be9a751004f084
lxy <442536013@qq.com>
2018-11-18 13:54:37 +0800

    687

---
+++ 687. Longest Univalue Path.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+"""
+Given a binary tree, find the length of the longest path where each node in the path has the same value. This path may or may not pass through the root.
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+Example 1:
+
+Input:
+
+              5
+             / \
+            4   5
+           / \   \
+          1   1   5
+Output:
+
+2
+Example 2:
+
+Input:
+
+              1
+             / \
+            4   5
+           / \   \
+          4   4   5
+Output:
+
+2
+Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def longestUnivaluePath(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        def process(node):
+            # 可以把这道题分为两个题，一是就边长，而是求路径长
+            if node is None:
+                return 0
+            left_length = process(node.left)
+            right_length = process(node.right)
+            left_arrow = right_arrow = 0
+            if node.left and node.left.val == node.val:
+                left_arrow = left_length + 1
+            if node.right and node.right.val == node.val:
+                right_arrow = right_length + 1
+            self.ans = max(self.ans, left_arrow + right_arrow)
+            return max(left_arrow, right_arrow)
+
+        self.ans = 0
+        process(root)
+        return self.ans
