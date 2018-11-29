d1939b14ed5ff6fa92931fa7a7f1af980acb224a
lxy <442536013@qq.com>
2018-11-29 20:27:19 +0800

    121

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -50,3 +50,19 @@ class Solution(object):
             min_price = min(min_price, price)
             max_profit = max(max_profit, price - min_price)
         return max_profit
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        # 遍历需要记录一个到目前为止的股票最小值
+        min_price = float('inf')
+
+        for price in prices:
+            max_profit = max(max_profit, price - min_price)
+            min_price = min(min_price, price)
+        return max_profit
