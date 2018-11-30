387ce3c38ad046c64d004bc424f1e2bdedbd727d
lxy <442536013@qq.com>
2018-11-30 13:41:25 +0800

    122

--- 122. Best Time to Buy and Sell Stock II.py
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -74,3 +74,80 @@ class Solution(object):
             peak = prices[index]
             max_profit += peak - valley
         return max_profit
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        """
+        第一个变量表示股市(0代表没有股市)，第二个变量表示最大交易次数，第三个变量表示手中股票库存
+        Base cases:
+        T[0][k][0] = 0 # 没有股市的时候没有利润
+        T[i][0][0] = 0 # 没有交易的时候没有利润
+        T[0][k][1] = -Infinity # 强调1如果没有库存或不允许交易，我们无法拥有库存
+        T[i][0][1] = -Infinity # 强调1如果没有库存或不允许交易，我们无法拥有库存
+
+        Recurrence relations:
+        T[i][k][0] = max(T[i-1][k][0], T[i-1][k][1] + prices[i])
+        T[i][k][1] = max(T[i-1][k][1], T[i-1][k-1][0] - prices[i])
+        每个交易的特点是两个动作成对出现 - 买入和卖出，并且由于“无多重交易”限制，这两者之间不能有其他行为交错。我们可以假设只有行动购买才会改变允许的最大交易数量,
+        为了找到最后一天结束时的最大利润，我们可以简单地遍历prices数组T[i][k][0]并T[i][k][1]根据上面的重复关系进行更新。最终的答案是T[i][k][0]（如果最终掌握0库存，我们总会有更大的利润）。
+
+        本题不限制交易次数，我们可以认为
+        k = len(prices) / 2 
+        """
+        k = len(prices) / 2
+        T = [[[0 for _ in range(2)] for _ in range(k + 1)] for _ in range(len(prices) + 1)]
+        for _k in range(k + 1):
+            T[0][_k][0] = 0
+            T[0][_k][1] = float('-inf')
+        for index in range(len(prices) + 1):
+            T[index][0][0] = 0
+            T[index][0][1] = float('-inf')
+        for index, price in enumerate(prices):
+            T[index + 1][k][0] = max(T[index][k][0], T[index][k][1] + price)
+            T[index + 1][k][1] = max(T[index][k][1], T[index][k][0] - price)
+        return T[len(prices)][k][0]
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        """
+        第一个变量表示股市(0代表没有股市)，第二个变量表示最大交易次数，第三个变量表示手中股票库存
+        Base cases:
+        T[0][k][0] = 0 # 没有股市的时候没有利润
+        T[i][0][0] = 0 # 没有交易的时候没有利润
+        T[0][k][1] = -Infinity # 强调1如果没有库存或不允许交易，我们无法拥有库存
+        T[i][0][1] = -Infinity # 强调1如果没有库存或不允许交易，我们无法拥有库存
+
+        Recurrence relations:
+        T[i][k][0] = max(T[i-1][k][0], T[i-1][k][1] + prices[i])
+        T[i][k][1] = max(T[i-1][k][1], T[i-1][k-1][0] - prices[i])
+        每个交易的特点是两个动作成对出现 - 买入和卖出，并且由于“无多重交易”限制，这两者之间不能有其他行为交错。我们可以假设只有行动购买才会改变允许的最大交易数量,
+        为了找到最后一天结束时的最大利润，我们可以简单地遍历prices数组T[i][k][0]并T[i][k][1]根据上面的重复关系进行更新。最终的答案是T[i][k][0]（如果最终掌握0库存，我们总会有更大的利润）。
+
+        本题不限制交易次数，我们可以认为
+        T[i-1][k-1][0] = T[i-1][k][0]
+        T[i-1][k-1][1] = T[i-1][k][1]
+
+        递归关系可以化简为
+        T[i][k][0] = max(T[i-1][k][0], T[i-1][k][1] + prices[i])
+        T[i][k][1] = max(T[i-1][k][1], T[i-1][k-1][0] - prices[i]) = max(T[i-1][k][1], T[i-1][k][0] - prices[i])
+
+        O(n)时间和O(1)空间
+        """
+        T_ik0 = 0
+        T_ik1 = float('-inf')
+
+        for index, price in enumerate(prices):
+            old_Tik0 = T_ik0
+            T_ik0 = max(T_ik0, T_ik1 + price)
+            T_ik1 = max(T_ik1, T_ik0 - price)
+        return T_ik0
