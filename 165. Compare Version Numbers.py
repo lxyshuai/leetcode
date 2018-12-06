1ab8b6c04473484772d4cf1affd0e1b36f364c7d
lxy <442536013@qq.com>
2018-12-06 17:41:56 +0800

    165

--- 165. Compare Version Numbers.py
+++ 165. Compare Version Numbers.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Compare two version numbers version1 and version2.
 If version1 > version2 return 1; if version1 < version2 return -1;otherwise return 0.
@@ -55,3 +56,41 @@ class Solution(object):
                 return -1
             version2_index += 1
         return 0
+
+
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        """
+        1.首先将version用split分开
+        2.从version1_list和version2_list的头部往后面遍历，如果遇到第一个不相同的，大的version大
+        3.如果有一个到了末尾，则看另外一个后面是不是全为0
+        """
+        version1_list = version1.split('.')
+        version2_list = version2.split('.')
+
+        for version1_list_index, version2_list_index in zip(range(len(version1_list)), range(len(version2_list))):
+            if int(version1_list[version1_list_index]) != int(version2_list[version2_list_index]):
+                return 1 if int(version1_list[version1_list_index]) > int(version2_list[version2_list_index]) else -1
+
+        if version1_list_index != len(version1_list) - 1:
+            version1_list_index += 1
+            while version1_list_index != len(version1_list):
+                if int(version1_list[version1_list_index]) != 0:
+                    return 1
+                version1_list_index += 1
+
+        if version2_list_index != len(version2_list) - 1:
+            version2_list_index += 1
+            while version2_list_index != len(version2_list):
+                if int(version2_list[version2_list_index]) != 0:
+                    return -1
+                version2_list_index += 1
+        return 0
+
+if __name__ == '__main__':
+    print Solution().compareVersion('1', '1.0.1')
