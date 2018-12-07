bccddd7d2331e32a98a38e74c12aabeb9a852eae
lxy <442536013@qq.com>
2018-12-07 17:28:23 +0800

    160

---
+++ 160. Intersection of Two Linked Lists.py
@@ -0,0 +1,89 @@
+# coding=utf-8
+"""
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+Credits:
+Special thanks to @stellari for adding this problem and creating all test cases.
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        first = headA
+        second = headB
+
+        while first and second:
+            first = first.next
+            second = second.next
+
+        if first is None:
+            first = headB
+        elif second is None:
+            second = headA
+
+        while first and second:
+            first = first.next
+            second = second.next
+
+        if first is None:
+            first = headB
+        elif second is None:
+            second = headA
+
+        while first and second:
+            if first is second:
+                return first
+            first = first.next
+            second = second.next
+        return None
+
+
+class Solution:
+    # @param two ListNodes
+    # @return the intersected ListNode
+    def getIntersectionNode(self, headA, headB):
+        if headA is None or headB is None:
+            return None
+
+        pa = headA  # 2 pointers
+        pb = headB
+
+        while pa is not pb:
+            # if either pointer hits the end, switch head and continue the second traversal,
+            # if not hit the end, just move on to next
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa  # only 2 ways to get out of the loop, they meet or the both hit the end=None
+
+# the idea is if you switch head, the possible difference between length would be countered.
+# On the second traversal, they either hit or miss.
+# if they meet, pa or pb would be the node we are looking for,
+# if they didn't meet, they will hit the end at the same iteration, pa == pb == None, return either one of them is the same,None
