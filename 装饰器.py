36e6559139b216bc2a913fba96e668868a4812ba
lxy <442536013@qq.com>
2018-12-11 10:10:11 +0800

    装饰器

---
+++ 装饰器.py
@@ -0,0 +1,37 @@
+import time
+from functools import wraps
+
+
+def decorate(limit_time, count):
+    def wrapper(func):
+        time_list = []
+
+        def inner_wrapper(*args, **kwargs):
+            now_time = time.time()
+            if len(time_list) == count:
+                if now_time - time_list[0] >= limit_time:
+                    time_list.pop(0)
+                    time_list.append(now_time)
+                    func(*args, **kwargs)
+                else:
+                    print 'fuck'
+            else:
+                time_list.append(now_time)
+                func(*args, **kwargs)
+
+        return inner_wrapper
+
+    return wrapper
+
+
+@decorate(3, 3)
+def say(something):
+    print something
+
+
+if __name__ == '__main__':
+    for _ in range(11):
+        say('Done')
+    time.sleep(3)
+    for _ in range(11):
+        say('Done')
