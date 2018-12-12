b3bd3f589e1afec334e7d083ac2c215ec7bea8ca
TENCENT\kobexyluo <kobexyluo@tencent.com>
2018-12-12 15:03:37 +0800

    304

--- 304. Range Sum Query 2D - Immutable.py
+++ 304. Range Sum Query 2D - Immutable.py
@@ -73,6 +73,54 @@ class NumMatrix(object):
         return self.dp[row2][col2] + (self.dp[row1 - 1][col1 - 1] if row1 > 0 and col1 > 0 else 0) - (
             self.dp[row2][col1 - 1] if col1 > 0 else 0) - (self.dp[row1 - 1][col2] if row1 > 0 else 0)
 
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        if matrix is None or not matrix:
+            return
+        rows = len(matrix)
+        columns = len(matrix[0])
+        self.dp = [[0 for _ in range(columns)] for _ in range(rows)]
+
+        # 初始化0,0
+        self.dp[0][0] = matrix[0][0]
+        # 初始化第一行
+        for _column in range(1, columns):
+            self.dp[0][_column] = self.dp[0][_column - 1] + matrix[0][_column]
+        # 初始化第一列
+        for _row in range(1, rows):
+            self.dp[_row][0] = self.dp[_row - 1][0] + matrix[_row][0]
+
+        for _row in range(1, rows):
+            for _column in range(1, columns):
+                self.dp[_row][_column] = self.dp[_row - 1][_column] + self.dp[_row][_column - 1] + matrix[_row][
+                    _column] - self.dp[_row - 1][_column - 1]
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        if row1 == 0 and col1 == 0:
+            return self.dp[row2][col2]
+        elif row1 == 0:
+            return self.dp[row2][col2] - self.dp[row2][col1 - 1]
+        elif col1 == 0:
+            return self.dp[row2][col2] - self.dp[row1 - 1][col2]
+        elif row1 >= 1 and col1 >= 1:
+            return self.dp[row2][col2] - self.dp[row2][col1 - 1] - self.dp[row1 - 1][col2] + self.dp[row1 - 1][col1 - 1]
+
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
